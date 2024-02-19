ebd7ac842bc60f4ccd07110b047af2626c7b1384
agent-indigo <bradenhynes@gmail.com>
2024-02-19 16:58:18 -0330

    Minor improvements

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,11 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
-const RootLayout = ({ children }) => {
+const layout = ({ children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
-export default RootLayout
+export default layout
