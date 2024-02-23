22fd02524a44fa0ac11c126aea17f7f47b15f4a0
agent-indigo <bradenhynes@gmail.com>
2024-02-22 22:00:41 -0330

    Add: category pagination

--- pages/blog/category/[category_name].jsx
+++ pages/blog/category/[category_name].jsx
@@ -4,20 +4,22 @@ import matter from 'gray-matter'
 import CategoryList from '@/components/CategoryList'
 import Layout from "@/components/Layout"
 import Post from '@/components/Post'
+import Pagination from '@/components/Pagination'
 import sortByDate from '@/utilities/sortByDate.mjs'
 import getPosts from '@/utilities/getPosts.mjs'
-const Category = ({posts, categoryName, categories}) => {
+const Category = ({ posts, categoryName, categories, currentPage, pageQty }) => {
   return (
     <Layout title={`${categoryName} | DevSpace`}>
       <div className="flex justify-between">
         <div className="w-3/4 mr-10">
           <h1 className='text-5xl border-b-4 p-5 font-bold' >Posts in {categoryName}</h1>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {posts && posts.map((post, index) => <Post key={index} post={post}/>)}
+            {posts && posts.map((post, index) => <Post key={index} post={post} />)}
           </div>
-          <div className="w-1/4">
-            <CategoryList categories={categories}/>
-          </div>
+          <Pagination currentPage={currentPage} pageQty={pageQty} />
+        </div>
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
         </div>
       </div>
     </Layout>
@@ -25,25 +27,28 @@ const Category = ({posts, categoryName, categories}) => {
 }
 export default Category
 export const getStaticPaths = async () => {
-    const files = fs.readdirSync(path.join('posts'))
-    const catgeories = files.map(filename => {
-        const MarkdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
-        const {data:frontMatter} = matter(MarkdownWithMeta)
-        return frontMatter.category.toLowerCase()
-    })
-    const paths = catgeories.map(category => ({params: {category_name: category}}))
-    return{paths, fallback: false}
+  const files = fs.readdirSync(path.join('posts'))
+  const categories = files.map(filename => {
+    const MarkdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+    const { data: frontMatter } = matter(MarkdownWithMeta)
+    return frontMatter.category.toLowerCase()
+  })
+  const paths = categories.map(category => ({ params: { category_name: category } }))
+  return { paths, fallback: false }
 }
-export const getStaticProps = async ({params: {category_name}}) => {
+export const getStaticProps = async ({ params: { category_name } }) => {
   const posts = getPosts()
-  const categories = posts.map(post => {post.frontMatter.category})
+  const categories = posts.map(post => post.frontMatter.category)
   const uniqueCategories = [...new Set(categories)]
   const postsInCategory = posts.filter(post => post.frontMatter.category.toLowerCase() === category_name)
+  const pageQty = Math.ceil(postsInCategory.length / process.env.POSTS_PER_PAGE)
   return {
     props: {
       posts: postsInCategory.sort(sortByDate),
       categoryName: category_name,
-      categories: uniqueCategories
+      categories: uniqueCategories,
+      currentPage: 1,
+      pageQty: pageQty
     }
   }
 }
